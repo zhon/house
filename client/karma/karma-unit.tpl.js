6215112cc42a51c101772019e47bfd094691c54f
zhon <zhon@xputah.org>
2013-08-15 15:34:19 -0600

    Tests run now

--- client/karma/karma-unit.tpl.js
+++ client/karma/karma-unit.tpl.js
@@ -16,7 +16,7 @@ module.exports = function ( karma ) {
     ],
 
     frameworks: [ 'jasmine' ],
-    plugins: [ 'karma-jasmine', 'karma-firefox-launcher', 'karma-chrome-launcher', 'karma-coffee-preprocessor' ],
+    plugins: [ 'karma-jasmine', /*'karma-firefox-launcher',*/ 'karma-chrome-launcher', 'karma-coffee-preprocessor' ],
     preprocessors: {
       '**/*.coffee': 'coffee',
     },
@@ -53,7 +53,7 @@ module.exports = function ( karma ) {
      * the aesthetic advantage of not launching a browser every time you save.
      */
     browsers: [
-      'Firefox'
+      'Chrome'
     ]
   });
 };
