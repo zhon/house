9f1cc74268033f3ff0ccdbf6a1f84d6a8459f108
zhon <zhon@xputah.org>
2013-09-26 16:30:32 -0600

    Unified t & ul post methods to /api/sales

--- server/app/sale/route.rb
+++ server/app/sale/route.rb
@@ -17,7 +17,7 @@ delete '/api/sale/:id' do
   ''
 end
 
-post '/api/t_sales' do
+post '/api/sales' do
   content_type :json
 
   JSON.parse(request.body.read, :symbolize_names => true).each do |item|
@@ -27,47 +27,3 @@ post '/api/t_sales' do
   ''
 end
 
-
-  # TODO fix bug with sale item & db sale 
-post '/api/ul_sales' do
-  #content_type :json
-  JSON.parse(request.body.read).each do |sale|
-     #find seller from phone or name
-    seller = Seller.or(
-      {phone: sale['seller_phone']},
-      {name: sale['seller']}
-    ).first
-
-    seller = Seller.create(
-      name: sale['seller'],
-      phone: sale['seller_phone']
-    ) unless seller
-
-    sale_date = Chronic.parse(sale['sale_date'])
-    sale = 
-      Sale.and( {:case => sale['case']}, {seller_id: seller.id}).first ||
-      Sale.and(
-               {address: sale['address']},
-               {owner: sale['owner']},
-               {seller_id: seller.id},
-               {county: sale['county']}
-              ).first ||
-      Sale.new(
-        seller_id: seller.id,
-        case: sale['case'],
-        address: sale['address'],
-        bid: sale['bid'],
-        url: sale['url'],
-        date: sale_date,
-        county: sale['county'],
-        owner: sale['owner']
-      )
-    now = Time.now
-    sale.update_attributes(
-      date: sale_date,
-      updated_at: now,
-    )
-    sale.save
-  end
-  ''
-end
