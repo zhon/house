5742da4c9a8318c41e6d6e71bc8575a319c9bfd5
zhon <zhon@xputah.org>
2013-10-14 16:36:36 -0600

    Update scraped_at test to use timecop

--- server/test/seller/seller_test.rb
+++ server/test/seller/seller_test.rb
@@ -3,18 +3,20 @@ require_relative '../helper'
 require_relative '../../app/seller/seller'
 require_relative '../../app/sale/sale'
 
+require 'timecop'
+
 
 describe Seller do
 
   describe 'update_scraped_at' do
 
     it 'sets scraped_at to Time.now' do
-      now = Time.new(2013,10,14)
-      seller = Seller.new
-      mock(Time).now { now }
-      mock(seller).scraped_at= now
-      mock(seller).save
-      seller.update_scraped_at
+      now = Time.new(2013, 10, 14)
+      Timecop.freeze(now) do
+        stub(seller = Seller.new).save
+        seller.update_scraped_at
+        seller.scraped_at.must_equal now
+      end
     end
 
   end
